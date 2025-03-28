80a196930e72c526c019e696f527ffedea64588d
Jordan73737 <jordanmctaggart7@hotmail.co.uk>
2025-03-28 10:34:29 +0000

    Fixed stockSlice.js to properly connect to production backend

--- src/slices/stocksSlice.js
+++ src/slices/stocksSlice.js
@@ -2,7 +2,8 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = `${import.meta.env.VITE_API_BASE_URL}/api`;
+
 const FINNHUB_API_KEY = "cv9n049r01qpd9s86e70cv9n049r01qpd9s86e7g";
 
 export const fetchFavorites = createAsyncThunk(
