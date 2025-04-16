0ca5a655ab348a75a442458c5e2446d47f02645c
Jordan73737 <jordanmctaggart7@hotmail.co.uk>
2025-04-16 21:10:18 +0100

    Added favourites button to home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import TradeSidebar from "./TradeSidebar"; // ✅ Import your TradeSidebar
+import handleToggleFavorite from "./Dashboard";
 
 const Home = () => {
   const [stocks, setStocks] = useState([]);
@@ -64,6 +65,31 @@ const Home = () => {
                   onClick={() => openTradeSidebar(stock, "sell")}
                   className="bg-red-600 text-white px-2 py-1 rounded">
                   Sell
+                </button>
+                <button
+                  onClick={() =>
+                    handleToggleFavorite(
+                      stock.symbol,
+                      stock.name,
+                      stock.favorite
+                    )
+                  }
+                  className="focus:outline-none mx-auto block">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill={stock.favorite ? "currentColor" : "none"}
+                    stroke="currentColor"
+                    className={`w-6 h-6 ${
+                      stock.favorite ? "text-yellow-400" : "text-gray-400"
+                    }`}>
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={stock.favorite ? 0 : 1.5}
+                      d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+                    />
+                  </svg>
                 </button>
               </td>
             </tr>
